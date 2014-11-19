b4f4f5d670fafd01d72fe276ed9f5939767fee00
csomu <chowdareddy.somu@proquest.com>
2014-11-19 12:46:50 -0500

    Apache cookbook with multiple with templates

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,2 +1,4 @@
 default["apache"]["indexfile"] = "index1.html"
 default["apache"]["serverfile"] = "/var/www/html/index.html"
+default["apache"]["sites"]["clowns"]={"port" => 80}
+default["apache"]["sites"]["bears"]={"port" => 81}
